e9b8eb685af0c90a9bb6239ca144d11f05d215b5
ujala <singhujala2004@gmail.com>
2025-02-09 11:01:26 +0530

    fix: ensure fallback values for Google provider credentials

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -5,8 +5,8 @@ import GoogleProvider from "next-auth/providers/google"
 const handler = NextAuth({
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      clientId: process.env.GOOGLE_CLIENT_ID! || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET! || "",
     }),
     GithubProvider({
       clientId: process.env.GITHUB_ID || "",
